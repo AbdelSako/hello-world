a462cc720842d6272e909fd28b64af381c52e92a
Abdel Sako <amsako90@gmail.com>
2023-06-17 14:10:56 -0400

    my_condition_variable

--- my_condition_variable_practice.hpp
+++ my_condition_variable_practice.hpp
@@ -4,38 +4,35 @@
 #include <mutex>
 #include <condition_variable>
 
+std::mutex m1, m2;
+std::condition_variable cv;
+bool ready;
 int i = 0;
-int* value = &i;
 
-class cl {
-public:
-	cl() = default;
-	void increase() {
-		++var;
-	}
-
-	int get() {
-		return var;
-	}
-
-private:
-	int var;
-};
-
-cl* c1 = new cl();
 
 void worker_thread(void) {
+	std::cout << "[+] worker_thread; ID = "
+		<< std::this_thread::get_id() << " ";
+
 	i++;
-	std::cout << "[+] Starting worker_thread...\n";
+	std::cout << "value = " << i << std::endl;
+	ready = true;
+	cv.notify_one();
 	return;
 }
 
 void Main(void) {
 	
 	std::thread thr(worker_thread);
-	std::cout << "[+] This expression appears before the detach().\n";
 	thr.detach();
+	{
+		std::unique_lock lk(m1);
+		cv.wait(lk, [] {return ready; });
+		ready = false;
+	}
+	
+	
 	std::cout << "processed value: " << i << std::endl;
-	delete c1;
+	//thr.join();
 	return;
 }
